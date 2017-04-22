449491203409ffda625fd4fcb356b2aa337ce6ce
Theresa Skonieczny <tsscamaro@gmail.com>
2017-04-22 18:13:50 -0400

    improved continue loop and validation

--- LabNum1/Program.cs
+++ LabNum1/Program.cs
@@ -10,38 +10,60 @@ namespace LabNum1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator! ");
+
+            //variables
+            Double Length = new Double();
+            Double Width = new Double();
+            
+            while (true)
             {
-            start:
-                Double Length = new Double();
-                Double Width = new Double();
+
+                Console.WriteLine("Please enter the Length of room: ");
+                //double is to allow decimals allows the user type decimal
+                Double.TryParse(Console.ReadLine(), out Length);
+              
+                Console.WriteLine("Please enter the Width of room and Hit Enter: ");
+                Double.TryParse(Console.ReadLine(), out Width);
+                //validation
+               
+
+                Console.WriteLine("The Area of the Room is: " + (Length * Width));
+                //double used to allow decimals within the the answer for perimeter
+                Double Perimeter = (Convert.ToDouble(Length) + Convert.ToDouble(Width)) * 2;
+                Console.WriteLine("The Perimeter of the Room is: " + Perimeter);
+                if (ContinueProgram())
                 {
-                    Console.WriteLine("Welcome to Grand Circus' Room Detail Generator! ");
-                    Console.WriteLine("Please enter the Length of room: ");
-                    //double is to allow decimals allows the user type decimal
-                    Double.TryParse(Console.ReadLine(), out Length);
-                    
-                    Console.WriteLine("Please enter the Width of room and Hit Enter: ");
-                    Double.TryParse(Console.ReadLine(), out Width);
-
-                    Console.WriteLine("The Area of the Room is: " + (Length * Width));
-                    //double used to allow decimals within the the answer for perimeter
-                    Double Perimeter = (Convert.ToDouble(Length) + Convert.ToDouble(Width)) * 2;
-                    Console.WriteLine("The Perimeter of the Room is: " + Perimeter);
-
-                    Console.WriteLine("Do you want to continue? Type 'Y' or 'N': ");
-                    string answerYOrN = Console.ReadLine();
-                    //if YOrN is the same as if YesOrNo
-                    if (answerYOrN == "y")
-                    {
-                        //returns to the beginning
-                        goto start;
-                    }
-                    if (answerYOrN == "n")
-                    {
-                        //exits the program
-                        return;
-                    }
+                    continue;
                 }
+                return;
+
+            }
+        }
+        public static void error()
+        {
+            Console.WriteLine("Input was not entered correctly. Try Again: ");
+        }
+        //Console.WriteLine("Do you want to continue? Type 'Y' or 'N': ");
+        public static bool ContinueProgram()
+        {
+            Console.WriteLine("\nContinue? (y / n): ");
+
+            string loop = Console.ReadLine().ToLower();
+            Console.WriteLine();
+            if (loop == "y")
+            {
+                return true;
+
+            }
+            if (loop == "n")
+            {
+                return false;
+            }
+            else
+            {
+                error();
+                return false;
             }
         }
     }
